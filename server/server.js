c5ecdef1f048ca76ac42206662b54e75fb7ec25f
Sakshi <sakshi.2428cseaiml13@kiet.edu>
2026-05-19 11:38:35 +0530

    Fixed CORS issue

--- server/server.js
+++ server/server.js
@@ -13,24 +13,9 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-const allowedOrigins = [
-  process.env.CLIENT_URL,
-  "http://localhost:5173",
-  "http://127.0.0.1:5173"
-].filter(Boolean);
-
-connectDB();
-
-app.use(helmet());
 app.use(
   cors({
-    origin(origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-        return;
-      }
-      callback(new Error("Not allowed by CORS"));
-    },
+    origin: "*",
     credentials: true
   })
 );
